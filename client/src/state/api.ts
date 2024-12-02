0ad86cc43461c2f749036907be519b8d4dff82aa
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-12-02 21:23:11 +0530

    created Search page and its components & redux API

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -61,6 +61,12 @@ export interface Task {
   attachments?: Attachment[];
 }
 
+export interface SearchResults {
+  tasks?: Task[];
+  projects?: Project[];
+  users?: User[];
+}
+
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
 
@@ -120,6 +126,12 @@ export const api = createApi({
         },
       ],
     }),
+
+    // Search -------------------------------//
+
+    search: build.query<SearchResults, string>({
+      query: (query) => `search?query=${query}`,
+    }),
   }),
 });
 
@@ -129,4 +141,5 @@ export const {
   useGetTasksQuery,
   useCreateTaskMutation,
   useUpdateTaskStatusMutation,
+  useSearchQuery,
 } = api;
