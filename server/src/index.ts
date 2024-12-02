27d07ad6364e4b9cc012084705862ed6b41d8b1c
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-12-02 21:22:21 +0530

    created search API

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,7 @@ import morgan from "morgan";
 //----- ROUTE IMPORTS ------//
 import projectRoutes from "./routes/projectRoute";
 import taskRoutes from "./routes/taskRoute";
+import searchRoutes from "./routes/searchRoute";
 
 /*------- CONFIGURATIONS ------*/
 dotenv.config();
@@ -27,6 +28,7 @@ app.get("/", (req, res) => {
 
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
+app.use("/search", searchRoutes);
 
 /*------- SERVER -------*/
 const port = process.env.PORT || 3000;
