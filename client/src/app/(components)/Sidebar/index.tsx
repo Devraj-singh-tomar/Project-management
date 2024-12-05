b1d0c5e8019799e9f3584a6f26b98b8d16911142
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-12-05 16:42:05 +0530

    uploaded images to S3

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -64,7 +64,12 @@ const Sidebar = () => {
 
         {/*---- Team -----*/}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
-          <Image src="/planning.png" alt="logo" width={40} height={40} />
+          <Image
+            src="https://pm-ss3-images.s3.us-east-1.amazonaws.com/planning.png"
+            alt="logo"
+            width={40}
+            height={40}
+          />
           <div className="">
             <h3 className="text-md font-bold uppercase tracking-wide dark:text-gray-200">
               pm team
